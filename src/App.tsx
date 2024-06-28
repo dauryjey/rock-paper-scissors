7f87f3055b7241dc7f6e0a5f3e2de02604ae0512
Daury P. <dauryjey@gmail.com>
2024-06-27 20:23:50 -0400

    chore: Add button styling to footer

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,15 @@ function App() {
           </div>
         </div>
       </header>
-      <main></main>
+      <main>
+        
+      </main>
       <footer>
-        <input type="button" value="RULES" />
+        <input
+          type="button"
+          value="RULES"
+          className="border-solid border-2 border-white tracking-widest font-medium px-6 py-2 rounded-lg text-xs"
+        />
       </footer>
     </div>
   )
